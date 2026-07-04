252d8aec57ff97e1e836e48230c84340f57bc600
Joel Manecas Soares <jmsoares@tic.gov.tl>
2026-07-04 15:50:14 +0900

    Removed allowed and trusted origin from source

--- config/settings/demo.py
+++ config/settings/demo.py
@@ -2,7 +2,8 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["demo-tdx.tualiqui.com"]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS').split(',')
 
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
